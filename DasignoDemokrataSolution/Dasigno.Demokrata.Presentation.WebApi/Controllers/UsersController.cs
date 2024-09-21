f5a05f3b1473967c3f34e46e752eb3716644da07
gustavoahc <gustavhc@gmail.com>
2024-09-20 20:11:16 -0500

    Primer commit - Estructura de proyectos aplicando Clean Architecture

---
+++ DasignoDemokrataSolution/Dasigno.Demokrata.Presentation.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dasigno.Demokrata.Presentation.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
